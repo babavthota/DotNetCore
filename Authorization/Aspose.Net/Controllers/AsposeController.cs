393d1bd59ceae9336e5d186d54969873314f9db3
BabaVThota <BabaVThota@MSI>
2021-06-30 19:34:55 -0400

    Aspose .net

---
+++ Authorization/Aspose.Net/Controllers/AsposeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aspose.Words;
+using Aspose.Words.Tables;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aspose.Net.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AsposeController : ControllerBase
+    {
+        
+        public IActionResult GetDocument()
+        {
+            Document document = new Document(@"wwwroot\AsposeDocument.docx");
+            DocumentBuilder builder = new DocumentBuilder(document);
+
+            builder.Writeln("Initial test document");
+            
+
+            return Ok(new { success = true });
+        }
+       
+    }
+}
